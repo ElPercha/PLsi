6fa7543c90845f6aadc6e982a82999f588f579c9
ElPercha <48414923+ElPercha@users.noreply.github.com>
2021-02-24 15:57:46 -0600

    License disclaimer

--- firm/include/Free_Fonts.h
+++ firm/include/Free_Fonts.h
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2019 Prieto Lucas. All rights reserved.
+This file is part of the PLsi project.
+
+PLsi is free software and hardware: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+PLsi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Attach this header file to your sketch to use the GFX Free Fonts. You can write
 // sketches without it, but it makes referencing them easier.
 
