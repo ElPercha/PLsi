c1fb028d88a1371442512fd973e08b8f3e9a51d2
ElPercha <funnyautomation@gmail.com>
2025-07-04 12:18:23 -0500

    Change License to MIT

--- firm/include/ladder.h
+++ firm/include/ladder.h
@@ -1,19 +1,5 @@
-/*
-Copyright (c) 2019 Prieto Lucas. All rights reserved.
-This file is part of the PLsi project.
-
-PLsi is free software and hardware: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-PLsi is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// Copyright (c) 2019 Lucas Prieto
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 #ifndef LADDER_H
 #define LADDER_H
